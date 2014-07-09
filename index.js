09637d3fedd51c5cc2c90476b1f3460eb7b74e95
Michael Bøcker-Larsen <mblarsen@gmail.com>
2014-07-09 17:24:48 +0545

    Fixed date bug and rewrote array iteration

--- index.js
+++ index.js
@@ -20,11 +20,7 @@ app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(session({
-  secret: 'whistleblown',
-  saveUninitialized: true,
-  resave: true
-}));
+app.use(session({ secret: 'whistleblown', saveUninitialized: true, resave: true }));
 
 var resume = {
   "bio": {
@@ -46,10 +42,13 @@ function makeDate(dateObj, isCurrent) {
   if (isCurrent !== undefined && isCurrent.valueOf() === true) {
     return '';
   }
-  if (dateObj.length === 3) {
+  if (dateObj.day) {
     return [dateObj.year, dateObj.month, dateObj.day].join('-');
   }
-  return [dateObj.year, dateObj.month, '01'].join('-');
+  if (dateObj.month) {
+    return [dateObj.year, dateObj.month].join('-');
+  }
+  return String(dateObj.year);
 }
 
 function makeResume($in) {
@@ -61,88 +60,65 @@ function makeResume($in) {
   resume.bio.websites.linkedIn = $in.publicProfileUrl;
   resume.bio.summary = $in.summary;
   
-  var i, max, collection, collectionKey, obj;
-  
-  // Recommendations
-  collection = $in.recommendationsReceived.values;
-  collectionKey = 'references';
+  var collection = $in.phoneNumbers.values;
   for (i = 0, max = collection.length; i < max; i += 1) {
-    obj = {
-      name: collection[i].recommender.firstName + ' ' + collection[i].recommender.lastName,
-      reference: collection[i].recommendationText
-    };
-    log('adding reference from ' + obj.name);
-    resume[collectionKey].push(obj);
+    log('adding phone ' + collection[i].phoneType + ' ' + collection[i].phoneNumber);
+    resume.bio['phone'][collection[i].phoneType] = collection[i].phoneNumber;
   }
 
-  // Publications
-  collection = $in.publications.values;
-  collectionKey = 'publications';
-  for (i = 0, max = collection.length; i < max; i += 1) {
-    obj = { 
-      name: collection[i].title,
-      releaseDate: makeDate(collection[i].date),
+  resume['references'] = $in.recommendationsReceived.values.map(function (obj) {
+    log('adding reference from ' + obj.recommender.firstName + ' ' + obj.recommender.lastName);
+    return {
+      name: obj.recommender.firstName + ' ' + obj.recommender.lastName,
+      reference: obj.recommendationText
+    };
+  });
+  
+  resume['publications'] = $in.publications.values.map(function (obj) {
+    log('adding publication ' + obj.title);
+    return { 
+      name: obj.title,
+      releaseDate: makeDate(obj.date),
       publisher: '',
       website: ''
     };
-    log('adding publication ' + obj.name);
-    resume[collectionKey].push(obj);
-  }
-
-  // Phones
-  collection = $in.phoneNumbers.values;
-  collectionKey = 'phone';
-  for (i = 0, max = collection.length; i < max; i += 1) {
-    log('adding phone ' + collection[i].phoneType + ' ' + collection[i].phoneNumber);
-    resume.bio[collectionKey][collection[i].phoneType] = collection[i].phoneNumber;
-  }
+  });
   
-  // Jobs
-  collection = $in.positions.values;
-  collectionKey = 'work';
-  for (i = 0, max = collection.length; i < max; i += 1) {
-    obj = {
-      company: collection[i].company.name,
-      position: collection[i].title,
+  resume['work'] = $in.positions.values.map(function (obj) {
+    log('adding work ' + obj.company.name);
+    return {
+      company: obj.company.name,
+      position: obj.title,
       website: '',
-      startDate: makeDate(collection[i].startDate),
-      endDate: makeDate(collection[i].startDate, Boolean(collection[i].isCurrent)),
-      summary: collection[i].summary,
+      startDate: makeDate(obj.startDate),
+      endDate: makeDate(obj.startDate, Boolean(obj.isCurrent)),
+      summary: obj.summary,
       highlights: []
-    };
-    log('adding work ' + obj.company);
-    resume[collectionKey].push(obj);
-  }
+    }
+  });
 
-  // Education
-  collection = $in.educations.values;
-  collectionKey = 'education';
-  for (i = 0, max = collection.length; i < max; i += 1) {
-    obj = {
-      institution: collection[i].schoolName,
-      startDate: makeDate(collection[i].startDate),
-      endDate: makeDate(collection[i].endDate, collection[i].isCurrent),
-      area: collection[i].fieldOfStudy,
-      summary: collection[i].notes,
-      studyType: collection[i].degree,
+  resume['education'] = $in.educations.values.map(function (obj) {
+    log('adding education ' + obj.schoolName);
+    return {
+      institution: obj.schoolName,
+      startDate: makeDate(obj.startDate),
+      endDate: makeDate(obj.endDate, obj.isCurrent),
+      area: obj.fieldOfStudy,
+      summary: obj.notes,
+      studyType: obj.degree,
       courses: []
     };
-    log('adding education ' + obj.studyType + '@' + obj.institution);
-    resume[collectionKey].push(obj);
-  }
+  });
   
-  // Skills
-  collection = $in.skills.values;
-  collectionKey = 'skills';
-  for (i = 0, max = collection.length; i < max; i += 1) {
-    obj = {
-      name: collection[i].skill.name,
+  resume['skills'] = $in.skills.values.map(function (obj) {
+    log('adding skill ' + obj.skill.name);
+    return {
+      name: obj.skill.name,
       level: 'beginner|intermediate|master',
       keywords: []
     };
-    log('adding skill ' + obj.name);
-    resume[collectionKey].push(obj);
-  }
+  });
+  
   return resume;
 };
 
@@ -183,7 +159,6 @@ app.get('/oauth/linkedin/callback', function(req, res) {
   console.log('Requesting access token');
   LinkedIn.auth.getAccessToken(res, req.query.code, function(err, data) {
     if (err) return console.error(err);
-    log(data);
     var result = JSON.parse(data);
     if (typeof result.error !== 'undefined') {
       req.session.error = result.error;
